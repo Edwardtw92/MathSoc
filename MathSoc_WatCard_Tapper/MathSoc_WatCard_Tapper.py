663048072188d6efb4ad7ea72767f07ab7cb3f5e
Edward Chang <edtw2003@gmail.com>
2023-06-09 10:54:22 -0400

    updated watcard scanner

--- MathSoc_WatCard_Tapper/MathSoc_WatCard_Tapper.py
+++ MathSoc_WatCard_Tapper/MathSoc_WatCard_Tapper.py
@@ -1,10 +1,10 @@
 from typing import Dict
 import time
 from playsound import playsound
-
-
+ 
+ 
 tapped_ids = {}
-
+ 
 while True:
   id = str(input('Tap WatCard: '))
   current_time = time.localtime()
@@ -12,11 +12,13 @@ while True:
   if current_time.tm_hour == 0 and current_time.tm_min == 0:
         # Reset the list of tapped IDs
         tapped_ids = {}
-
+ 
   if id in tapped_ids:
     if tapped_ids[id] >= 2:
         playsound('Portal2-BuzzerSound.mp3')
         print(tapped_ids[id])
+    else:
+        playsound('Portal2-Beep.mp3')
     tapped_ids[id] += 1
   else:
     playsound('Portal2-Beep.mp3')
